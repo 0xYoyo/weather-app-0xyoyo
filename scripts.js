abd3bc7a4720ad50879575e4f9e116ef0deff2bc
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2023-03-19 18:43:40 +0200

    populate frontend

--- scripts.js
+++ scripts.js
@@ -10,13 +10,34 @@ async function getWeather(city) {
       cityName: responseJson.name + ", ",
       countryCode: responseJson.sys.country,
       temperature: responseJson.main.temp + "° Celsius",
-      windSpeed: responseJson.wind.speed,
+      windSpeed: "Wind: " + responseJson.wind.speed + " m/s",
       state: responseJson.weather[0].description,
     };
     console.log(weatherObj);
     return weatherObj;
   } catch (error) {
     console.log(error);
+    alert("Please try again with a different city");
   }
 }
-getWeather("tel aviv");
+
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+btn.addEventListener("click", async function (event) {
+  const cityValue = input.value;
+  const obj = await getWeather(cityValue);
+  const location = document.querySelector("#location");
+  const temp = document.querySelector("#temp");
+  const speed = document.querySelector("#speed");
+  const description = document.querySelector("#description");
+  location.textContent = obj.cityName + obj.countryCode;
+  temp.textContent = obj.temperature;
+  speed.textContent = obj.windSpeed;
+  description.textContent = obj.state;
+});
+
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    btn.click();
+  }
+});
